d9abe2d6752e27d77f04d40b6775ca63bbbcc673
Syefoyo <trisyefolionel@gmail.com>
2024-05-15 18:28:18 +0700

    morninginc

---
+++ script.js
@@ -0,0 +1,10 @@
+function colorOK() {
+    let x = document.getElementById("test")
+    x.style.backgroundColor = "lightgreen"
+    console.log("Correct")
+}
+function colorOk1() {
+    let o = document.getElementById("test2")
+    o.style.backgroundColor = "red"
+    console.log("Incorrect")
+}
